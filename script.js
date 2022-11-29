05f5f74e2f773dbd59f8b895ae973676011e91eb
Rainy <112306653+rainy-moss@users.noreply.github.com>
2022-11-29 14:52:44 -0600

    completed assignment

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ window.addEventListener("load", function() {
     fetch('https://handlers.education.launchcode.org/static/astronauts.json').then
     (function(response){
        response.json().then(function(data){
-        for (let i=0; i > data.length; i++) {
+        console.log(data);
+        for (let i=0; i < data.length; i++) {
             let astronaut = data[i];
             container.innerHTML +=`
             <div class="astronaut">
@@ -16,7 +17,7 @@ window.addEventListener("load", function() {
                <ul>
                   <li>Hours in space: ${astronaut.hoursInSpace}</li>
                   <li>Active: ${astronaut.active}</li>
-                  <li>Skills: Physician, Chemical Engineer</li>
+                  <li>Skills: ${astronaut.skills.join(", ")}</li>
                </ul>
             </div>
             <img class="avatar" src="${astronaut.picture}">
